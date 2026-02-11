6d780236d31cde3f903fc4c9c8b55af847caf785
Andres Lopez <dnmega2500@gmail.com>
2026-02-11 16:46:03 -0500

    fix

--- src/pages/ImageLoader.tsx
+++ src/pages/ImageLoader.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Box from '@mui/material/Box';
 import { Hearts } from 'react-loader-spinner';
+import { Typography } from '@mui/material';
 
 const ImageLoader = ({ src, alt }: { src: string, alt: string }) => {
     const [status, setStatus] = useState('loading'); // 'loading', 'loaded', 'failed'
@@ -9,38 +10,21 @@ const ImageLoader = ({ src, alt }: { src: string, alt: string }) => {
     const handleError = () => setStatus('failed');
 
     return (
-        <Box sx={{ position: 'relative' }}>
+        <Box sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+        }}>
             {status === 'loading' && (
-                <Box
-                    sx={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        // Use Skeleton for a content-anticipating loading UI
-                        // or CircularProgress for a general spinner
-                    }}
-                >
+                <Box sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}>
+                    <Typography variant="h6">{"Espera un ratito XD"}</Typography>
                     <Hearts color="#ff80bf" height={200} width={200} />
-                    {/* <CircularProgress /> */}
                 </Box>
             )}
-
-            {status === 'failed' && (
-                <Box
-                    sx={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        width: '100%',
-                        height: '100%',
-                        border: '1px solid red',
-                        color: 'red',
-                    }}
-                >
-                    Error loading image
-                </Box>
-            )}
-
             {/* The actual image element */}
             <Box
                 component="img"
